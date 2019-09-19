1d347008a12b936c9c7d0ba306e782a103346dae
Nathan Roy <nathanmroy@yahoo.com>
2019-09-18 20:52:22 -0700

    linked to port; need to connect data to html

--- db.js
+++ db.js
@@ -1,68 +1,69 @@
 const Sequelize = require('sequelize');
 const { TEXT, STRING, DECIMAL, UUID, UUIDV4 } = Sequelize;
-const connection = new Sequelize('postgres://localhost/acme_products_offering')
+const connection = new Sequelize('postgres://localhost/acme_products_offering');
 
 const Product = connection.define('product', {
-    id: {
-        type: UUID,
-        primaryKey: true,
-        defaultValue: UUIDV4
-    },
-    name: STRING,
-    suggestedPrice: DECIMAL
+	id: {
+		type: UUID,
+		primaryKey: true,
+		defaultValue: UUIDV4
+	},
+	name: STRING,
+	suggestedPrice: DECIMAL
 });
 
 const Company = connection.define('company', {
-    id: {
-        type: UUID,
-        primaryKey: true,
-        defaultValue: UUIDV4
-    },
-    name: STRING
+	id: {
+		type: UUID,
+		primaryKey: true,
+		defaultValue: UUIDV4
+	},
+	name: STRING
 });
 
 const Offering = connection.define('offering', {
-    id: {
-        type: UUID,
-        primaryKey: true,
-        defaultValue: UUIDV4
-    },
-    price: DECIMAL
+	id: {
+		type: UUID,
+		primaryKey: true,
+		defaultValue: UUIDV4
+	},
+	price: DECIMAL
 });
 
-const mapPromise = ( items, model ) => {
-    return Promise.all(items.map(item => model.create(item)));
+Offering.belongsTo(Product);
+Product.hasMany(Offering);
+Offering.belongsTo(Company);
+Company.hasMany(Offering);
+
+const mapPromise = (items, model) => {
+	return Promise.all(items.map((item) => model.create(item)));
 };
 
 const syncOrSwim = async () => {
-    await connection.sync({ force: true});
-    const products = [
-        {name: 'Dopamine pills', suggestedPrice: 1.99 },
-        {name: 'Paper', suggestedPrice: 100.00 },
-        {name: 'Rabbit"s feet', suggestedPrice: 45.00 }
-    ];
-    const [dP, paper, rF] = await mapPromise(products, Product);
+	await connection.sync({ force: true });
+	const products = [
+		{ name: 'Dopamine pills', suggestedPrice: 1.99 },
+		{ name: 'Paper', suggestedPrice: 100.0 },
+		{ name: "Rabbit's feet", suggestedPrice: 45.0 }
+	];
+	const [ dP, paper, rF ] = await mapPromise(products, Product);
 
-    const companies = [
-        {name: 'Rite Aid'},
-        {name: 'Staples'},
-        {name: 'Bob"s Rabbit Store'}
-    ];
-    const [rA, staples, bRS] = await mapPromise(companies, Company);
+	const companies = [ { name: 'Rite Aid' }, { name: 'Staples' }, { name: "Bob's Rabbit Store" } ];
+	const [ rA, staples, bRS ] = await mapPromise(companies, Company);
 
-    const offerings = [
-        {price: 2.50},
-        {price: 110.00},
-        {price: 48.33}
-    ];
-    await mapPromise(offerings, Offering)
-}
+	const offerings = [
+		{ price: 2.5, productId: dP.id, companyId: rA.id },
+		{ price: 110.0, productId: paper.id, companyId: staples.id },
+		{ price: 48.33, productId: rF.id, companyId: bRS.id }
+	];
+	await mapPromise(offerings, Offering);
+};
 
 module.exports = {
-    syncOrSwim,
-    models: {
-        Product,
-        Company,
-        Offering
-    }
-}
+	syncOrSwim,
+	models: {
+		Product,
+		Company,
+		Offering
+	}
+};
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const {syncOrSwim, models: {Product, Company, Offering}} = require('./db');
+const { syncOrSwim, models: { Product, Company, Offering } } = require('./db');
 const app = express();
 const path = require('path');
 
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
 
-syncOrSwim()
-    .then(()=> {
-        const port = process.env.PORT || 3000;
-        app.listen(port, ()=> console.log(`0 to ${port}, real quick`))
-        
-    })
+//app.use('/api', require('./api'));
+
+syncOrSwim().then(() => {
+	const port = process.env.PORT || 3000;
+	app.listen(port, () => console.log(`0 to ${port}, real quick`));
+});
